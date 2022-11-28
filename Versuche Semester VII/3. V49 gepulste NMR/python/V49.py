88e524ca05ed6e4690fb782e19d6c11ee1c8f13d
benedikt <benedikt.sander@tu-dortmund.de>
2022-11-28 22:40:12 +0100

    lil' update

--- Versuche Semester VII/3. V49 gepulste NMR/python/V49.py
+++ Versuche Semester VII/3. V49 gepulste NMR/python/V49.py
@@ -9,6 +9,9 @@ import sympy
 import os
 from tabulate import tabulate
 from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+from tabulate import tabulate
+from scipy.signal import find_peaks
+
 
 
 if os.path.exists("build") == False:
@@ -47,9 +50,9 @@ t_2, U_2 = np.genfromtxt("python/data/scope_1.csv",skip_header= 2, skip_footer= 
 #Diffusionsmessung
 
 t_diff = np.array([0.1, 0.5, 1, 2.5, 5, 7.5, 10, 12.5, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]) #in ms
-U_diff = np.array([1.5, 1.487, 1.487, 1.468, 1.375, 1.15, 0.837, 0.475, 0.212, 0.160, 0.110, 0.073, 0.047, 0.041, 0.033, 0.029, 0.02, 0.027]) # in V
+U_diff = np.array([1.5, 1.487, 1.487, 1.468, 1.375, 1.15, 0.837, 0.475, 0.212, 0.160, 0.110, 0.073, 0.047, 0.041, 0.033, 0.029, 0.024, 0.02, 0.027]) # in V
 
-tau5_diff, Real_diff, Imag_diff = np.genfromtxt("python/data/scope_4.csv",skip_header= 5, unpack = True)
+tau5_diff, Real_diff, Imag_diff = np.genfromtxt("python/data/echo_gradient.csv",skip_header= 5, unpack = True)
 
 
 #####FUNKTIONEN#######
@@ -59,17 +62,33 @@ def rel_abw(theo,a):
     print(f"Relative Abweichung in Prozent: {noms(c) * 100 :.4f} \pm {stds(c) * 100 :.5f}\n")
 
 
+def printer(a,b,c,d,name):
+    print(f"### {name} ###")
+    table1 ={'Messreihe 1': a, 'Messreihe 2': b,  'Messreihe 3': c, 'Messreihe 4': d }
+    print("\n", tabulate(table1, tablefmt = "latex_raw"))  
 
+def printer(a,b,name):
+    print(f"### {name} ###")
+    table1 ={'Messreihe 1': a, 'Messreihe 2': b }
+    print("\n", tabulate(table1, tablefmt = "latex_raw"))  
 
 
 #####RECHNUNGEN#######
 
+
+
+
+
+
+
+
 ###### T1-Auswertung #####
 print("\n\t##### T1- Auswertung ######")
-def fit_T1(t, a, b, c):
+
+def fit_exp(t, a, b, c):
     return a*(1-2*np.exp(-t/b))+c
 
-params1, cov1 = curve_fit(fit_T1, t_1, U_1, p0 = (-1.6, 2839, -0.638 ))
+params1, cov1 = curve_fit(fit_exp, t_1, U_1, p0 = (-1.6, 2839, -0.638 ))
 cov1 = np.sqrt(np.diag(abs(cov1)))
 
 uparams1 = unp.uarray(params1, cov1)
@@ -81,7 +100,7 @@ x1 = np.linspace(t_1[0], t_1[-1], 1000)
 
 plt.figure()
 plt.plot(t_1, U_1,"x",label="Messwerte")
-plt.plot(x1, fit_T1(x1, *params1))
+plt.plot(x1, fit_exp(x1, *params1), label = "Fit-Funktion")
 #plt.xscale('log')
 plt.xlabel(r"$\tau$ $/$ $ms$")
 plt.ylabel(r"$U$ $/$ $V$")
@@ -94,19 +113,93 @@ plt.savefig("build/plots/T1.pdf")
 
 
 
-#####RECHNUNGEN#######
+
+
+
+
+
 
 ###### T2-Auswertung #####
 print("\n\t##### T2- Auswertung ######")
-def fit_T1(t, a, b, c):
-    return a*(1-2*np.exp(-t/b))+c
 
-params1, cov1 = curve_fit(fit_T1, t_1, U_1, p0 = (-1.6, 2839, -0.638 ))
-cov1 = np.sqrt(np.diag(abs(cov1)))
 
-uparams1 = unp.uarray(params1, cov1)
+U_2_peaks, peak_heights = find_peaks(U_2, height = 0.25)
+t_2_new = t_2[U_2_peaks]
+U_2_new = U_2[U_2_peaks]
 
-print(f"U_0 = {noms(uparams1[0]):.4f} \pm {stds(uparams1[0]):.4f} Volt\nT1 = {noms(uparams1[1]):.4f} \pm {stds(uparams1[1]):.4f} ms\nU_1 = {noms(uparams1[2]):.4f} \pm {stds(uparams1[2]):.4f} Volt\n")
+params2, cov2 = curve_fit(fit_exp,2 * t_2_new, U_2_new, p0 = (-0.44, 1.97, 0.4 ))
+cov2 = np.sqrt(np.diag(abs(cov2)))
+uparams2 = unp.uarray(params2, cov2)
+
+print(f"U_0 = {noms(uparams2[0]):.4f} \pm {stds(uparams2[0]):.4f} Volt\nT2 = {noms(uparams2[1]):.4f} \pm {stds(uparams2[1]):.4f} s\nU_1 = {noms(uparams2[2]):.4f} \pm {stds(uparams2[2]):.4f} Volt\n")
+
+x2 = np.linspace(t_2_new[0], t_2_new[-1])
+
+plt.figure()
+#plt.plot(t_2, U_2,".",label="Messwerte")
+plt.plot(t_2_new, U_2_new, "x", label = "Peaks")
+plt.plot(x2, fit_exp(2 * x2, *params2), label = "Fit-Funktion")
+#plt.xscale('log')
+plt.xlabel(r"$\tau$ $/$ $s$")
+plt.ylabel(r"$U$ $/$ $V$")
+plt.tight_layout()
+plt.legend()
+plt.savefig("build/plots/T2.pdf")
+
+
+
+
+
+
+#######DIFFUSIONSKONSTANTE#########
+print("\n\t##### Dffusionskonstante ######")
+
+
+G = 0.1203  #aus der fourier.py
+gyro = 2.67*10**8 #für Protonen T/s
+global T2 
+T2 = uparams2[1] * 10**(3) # in ms
+
+def fit_diff(t, a, b, c):
+    #T2 = 3.9523
+    T_2 = 3.9523 * 10**(3)
+    return a * np.exp(-(2*t)/T_2) * np.exp(-t**3/b)+c
+
+params3, cov3 = curve_fit(fit_diff, t_diff, U_diff, p0 = (1.46, 1677, 0.026))
+cov3 = np.sqrt(np.diag(abs(cov3)))
+uparams3 = unp.uarray(params3, cov3)
+
+D = 3 / (2 * uparams3[1] * 10**(-9) * gyro**2 * G**2) * 10**9
+
+print(f"U_0 = {noms(uparams3[0]):.4f} \pm {stds(uparams3[0]):.4f} Volt\nT3 = {noms(uparams3[1]):.4f} \pm {stds(uparams3[1]):.4f} (ms)^3\nU_1 = {noms(uparams3[2]):.4f} \pm {stds(uparams3[2]):.4f} Volt\n")
+print(f"D = {noms(D):.4f} \pm {stds(D):.4f}")
+x3 = np.linspace(t_diff[0], t_diff[-1])
+
+plt.figure()
+#plt.plot(t_2, U_2,".",label="Messwerte")
+plt.plot(t_diff, U_diff, "x", label = "Messwerte")
+plt.plot(x3, fit_diff(x3, *params3), label = "Fit-Funktion")
+#plt.xscale('log')
+plt.xlabel(r"$\tau$ $/$ $ms$")
+plt.ylabel(r"$U$ $/$ $V$")
+plt.tight_layout()
+plt.legend()
+plt.savefig("build/plots/diff1.pdf")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -114,14 +207,12 @@ print(f"U_0 = {noms(uparams1[0]):.4f} \pm {stds(uparams1[0]):.4f} Volt\nT1 = {no
 
 
 
-plt.figure()
-plt.plot(t_2, U_2,"x",label="Messwerte")
-#plt.xscale('log')
-plt.xlabel(r"$\tau$ $/$ $ms$")
-plt.ylabel(r"$U$ $/$ $V$")
-#plt.xscale("log")
-#plt.xticks([5*10**3,10**4,2*10**4,4*10**4],[r"$5*10^3$", r"$10^4$", r"$2*10^4$", r"$4*10^4$"])
-#plt.yticks([0,np.pi/8,np.pi/4,3*np.pi/8,np.pi/2],[r"$0$",r"$\frac{\pi}{8}$", r"$\frac{\pi}{4}$",r"$\frac{3\pi}{8}$", r"$\frac{\pi}{2}$"])
-plt.tight_layout()
-plt.legend()
-plt.savefig("build/plots/plot2.pdf")
+
+######MESSDATEN##########
+
+
+#printer(t_2[U_2_peaks], U_2[U_2_peaks] ,"peaks")
+#printer(t_diff, U_diff ,"Diffusion")
+
+
+
---
+++ Versuche Semester VII/3. V49 gepulste NMR/python/fourier.py
@@ -0,0 +1,87 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+import scipy.constants as const
+import sympy
+import os
+from tabulate import tabulate
+from uncertainties.unumpy import (nominal_values as noms, std_devs as stds)
+from tabulate import tabulate
+from scipy.signal import find_peaks
+
+
+#Laden der Daten aus der Datei "echo_gradient.csv" 
+#Die erste Spalte enthält die Zeiten in Sekunden, die zweite Spalte  
+#den Realteil und die dritte Spalte den Imaginärteil 
+times, real, imag= np.genfromtxt("python/data/echo_gradient.csv", skip_header= 5, unpack = True)
+
+#Suchen des Echo-Maximums und alle Daten davor abschneiden 
+start = np.argmax(real) 
+times = times[start:] 
+real = real[start:] 
+imag = imag[start:] 
+#Phasenkorrektur - der Imaginärteil bei t=0 muss = 0 sein 
+phase = np.arctan2(imag[0], real[0]) 
+#Daten in komplexes Array mit Phasenkorrektur speichern 
+compsignal = (real*np.cos(phase)+imag*np.sin(phase))+ (-real*np.sin(phase)+imag*np.cos(phase))*1j 
+#Offsetkorrektur, ziehe den Mittelwert der letzten 512 Punkte von allen Punkten ab 
+compsignal = compsignal - compsignal[-512:-1].mean() 
+#Der erste Punkt einer FFT muss halbiert werden 
+compsignal[0] = compsignal[0]/2.0 
+#Anwenden einer Fensterfunktion (siehe z. Bsp. #https://de.wikipedia.org/wiki/Fensterfunktion ) 
+#Hier wird eine Gaußfunktion mit sigma = 100 Hz verwendet 
+apodisation = 100.0*2*np.pi 
+compsignal = compsignal*np.exp(-1.0/2.0*((times-times[0])*apodisation)**2) 
+#Durchführen der Fourier-Transformation 
+fftdata = np.fft.fftshift(np.fft.fft(compsignal)) 
+#Generieren der Frequenzachse 
+freqs = np.fft.fftshift(np.fft.fftfreq(len(compsignal), times[1]-times[0])) 
+#Speichern des Ergebnisses als txt 
+np.savetxt("build/plots/echo_gradient_fft.txt", np.array([freqs, np.real(fftdata), np.imag(fftdata)]).transpose()) 
+#Erstellen eines Plots 
+
+
+
+
+
+
+
+
+#######MINE########
+
+
+
+peaks, peak_heights = find_peaks( -1 * np.real(fftdata), height = 18)
+print(np.real(fftdata)[peaks])
+
+
+
+lower = 5050
+upper = 5200
+plt.plot(freqs[lower:upper], np.real(fftdata)[lower:upper], label = "Fouriertransformierte Daten") 
+plt.plot(freqs[peaks], np.real(fftdata)[peaks], label = "Peakbreite" )
+plt.xlabel(r"$f$ $/$ $Hz$")
+plt.ylabel(r"")
+plt.tight_layout()
+plt.legend()
+plt.savefig("build/plots/echo_gradient.pdf")
+
+#plt.axis([0,17500,-7,30])
+
+####G berechnen
+
+d = 4.2*10**-3 # in m
+gyro = 2.67*10**8 #für Protonen T/s
+d_f = 0 #Durchmesser Spektrum 
+
+
+peaks, peak_heights = find_peaks( -1 * np.real(fftdata), height = 18)
+print(np.real(fftdata)[peaks])
+
+d_f = freqs[peaks[1]] - freqs[peaks[0]] #Durchmesser Spektrum 
+
+G = (2 * np.pi * d_f)/(gyro * d)
+print(f"\nVerteilungsdicke d_f = {d_f:.4f}\nGradientenstärke G = {G:.4f}")
