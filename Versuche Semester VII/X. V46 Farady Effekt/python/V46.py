f476ee6ea35dcbe15c38d4936af56fa987477fb1
Jana Schlücking <jana.schluecking@tu-dortmund.de>
2022-12-13 02:51:32 +0100

    really want to sleep

--- Versuche Semester VII/X. V46 Farady Effekt/python/V46.py
+++ Versuche Semester VII/X. V46 Farady Effekt/python/V46.py
@@ -30,9 +30,9 @@ N_28 = 2.8e22 #\per\metre\cubic
 
 #####Funktionen#######
 
-def fortable(eins, zwei, drei, vier):
+def fortable(eins, zwei, drei, vier, fünf):
     for i in range(len(eins)):
-        print(f'{eins[i]:.3f} & {zwei[i]:.3f} & {drei[i]:.3f} & {vier[i]:.3f} \ \ \n ')
+        print(f'{eins[i]:.3f} & {zwei[i]:.3f} & {drei[i]:.3f} & {vier[i]:.3f} & {fünf[i]:.3f} \ \ \n ')
     return
 
 def line(x, a, b):
@@ -57,14 +57,16 @@ print(f'maximales B-Feld: {B_max} mT\n')
 plt.figure()
 plt.plot(dz, abs(B), 'k.', label='Messwerte')
 plt.hlines(B_max, -10, 10, color='red', linestyle='--', label='maximale Feldstärke')
-# plt.xlabel(r'$z $/$ \si{\milli\metre}$')
-# plt.ylabel(r'$B $/$ \si{\milli\tesla}$')
+plt.xlabel(r'$z $/$ \si{\milli\metre}$')
+plt.ylabel(r'$B $/$ \si{\milli\tesla}$')
 plt.legend()
 plt.tight_layout()
 plt.savefig('build/plots/bfeld.pdf')
 
+fortable(dz, B, np.zeros(len(B)), np.zeros(len(B)), np.zeros(len(B)))
+
 print("____________________________________________________________REINES GaAs______________________________________________________________________\n")
-def firstplot(name, lab, col):
+def firstplot(name, lab, col, L_probe):
     lamb, t1, wm1, t2, wm2 = np.genfromtxt('python/data/'+name+'.txt', unpack=True)
 
     theta_1 = (t1 + wm1/60)*(2*np.pi/360)   # in radiant
@@ -76,7 +78,7 @@ def firstplot(name, lab, col):
     plt.plot(lamb**2, theta, '.', color=col, label='Messwerte '+lab+'')
     plt.xlabel(r'$\lambda^2 $/$ \si{\micro\metre\squared}$')
     plt.ylabel(r'$\theta$')
-    plt.xticks(lamb**2, ['$1.06^2$', '$1.29^2$', '$1.45^2$', '$1.72^2$', '$1.96^2$', '$2.156^2$', '$2.34^2$', '$2.51^2$', '$2.65^2$'])
+    plt.xticks(lamb**2, ['$1.06^2$', '$1.29^2$', '$1.45^2$', '$1.72^2$', '$1.96^2$', '$2.156^2$', '$2.34^2$', '$2.51^2$', '$2.56^2$'])
     # plt.xlabel('lamb / microm^2')
     # plt.ylabel('theta')
     plt.legend()
@@ -84,16 +86,29 @@ def firstplot(name, lab, col):
     plt.savefig('build/plots/'+name+'.pdf')
 
     print('___________________________'+name+'_____________'+lab+'_______________________')
-    fortable(lamb, theta_1, theta_2, theta)
+    fortable(lamb, theta_1, theta_2, theta, theta/L_probe)
     return theta
 
-theta_rein = firstplot('hochrein', 'reines GaAs', 'black')
+theta_rein = firstplot('hochrein', 'reines GaAs', 'black', L_rein)
 
-theta_12 = firstplot('probe1', 'Probe 1', 'red')      #r'$N=\SI{1.2e18}{1\per\centi\metre\tothe{3}$',
-theta_28 = firstplot('probe2', 'Probe 2', 'orange')   #r'$N=\SI{2.8e18}{1\per\centi\metre\tothe{3}$',
+theta_12 = firstplot('probe1', 'Probe 1', 'red', L_12)      #r'$N=\SI{1.2e18}{1\per\centi\metre\tothe{3}$',
+theta_28 = firstplot('probe2', 'Probe 2', 'orange', L_28)   #r'$N=\SI{2.8e18}{1\per\centi\metre\tothe{3}$',
 
 
-lamlam = np.array([1.06, 1.29, 1.45, 1.72, 1.96, 2.156, 2.34, 2.51, 2.65])
+lamlam = np.array([1.06, 1.29, 1.45, 1.72, 1.96, 2.156, 2.34, 2.51, 2.56])
+
+plt.figure()
+plt.plot(lamlam**2, theta_rein/L_rein, '.', color='black', label=r'$\che{GaAs}$')
+plt.plot(lamlam**2, theta_12/L_12, '.', color='red', label=r'$\che{InGaAs}, N = \SI{1.2e18}{\per\centi\metre\tothe{3}}$ ')
+plt.plot(lamlam**2, theta_28/L_28, '.', color='orange', label=r'$\che{InGaAs}, N = \SI{2.8e18}{\per\centi\metre\tothe{3}$')
+plt.xlabel(r'$\lambda^2 $/$ \si{\micro\metre\squared}$')
+plt.ylabel(r'$\theta_\text{frei} $/$ \si{1\per\metre}$')
+plt.xticks(lamlam**2, ['$1.06^2$', '$1.29^2$', '$1.45^2$', '$1.72^2$', '$1.96^2$', '$2.156^2$', '$2.34^2$', '$2.51^2$', '$2.56^2$'])
+# plt.xlabel('lamb / microm^2')
+# plt.ylabel('theta')
+plt.legend()
+plt.tight_layout()
+plt.savefig('build/plots/firstplot.pdf')
 
 
 def secondplot(name, theta_probe, L_probe, N_probe, col):
@@ -114,7 +129,7 @@ def secondplot(name, theta_probe, L_probe, N_probe, col):
     plt.plot(l_plot, line(l_plot, a.n, b.n), 'k--', label='Ausgleichsrechnung')
     plt.ylabel(r'$\theta_\text{frei} $/$ \si{\per\metre}$')
     plt.xlabel(r'$\lambda^2 $/$ \si{\micro\metre}$')
-    plt.xticks(lamlam**2,  ['$1.06^2$', '$1.29^2$', '$1.45^2$', '$1.72^2$', '$1.96^2$', '$2.156^2$', '$2.34^2$', '$2.51^2$', '$2.65^2$'])
+    plt.xticks(lamlam**2,  ['$1.06^2$', '$1.29^2$', '$1.45^2$', '$1.72^2$', '$1.96^2$', '$2.156^2$', '$2.34^2$', '$2.51^2$', '$2.56^2$'])
     plt.legend()
     plt.tight_layout()
     plt.savefig('build/plots/'+name+'_diff.pdf')
@@ -122,7 +137,7 @@ def secondplot(name, theta_probe, L_probe, N_probe, col):
     print(N_probe)
     m_eff = effmass(abs(a), N_probe)
 
-    print(f'effektive Masse m* = {m_eff} kg')
+    print(f'effektive Masse m* = {m_eff} kg = {m_eff/const.m_e} m_e')
     return
 
 secondplot('probe1', theta_12, L_12, N_12, 'red')
